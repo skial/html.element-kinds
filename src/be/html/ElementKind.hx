cfee487c57be61212d293443fbe36e7e453c36b0
Bren <skial.bainn@gmail.com>
2021-05-11 09:30:58 +0100

    Initial simple code.

--- src/be/html/ElementKind.hx
+++ src/be/html/ElementKind.hx
@@ -1,9 +1,9 @@
-# html.element-kinds
+package be.html;
 
-A simple enum abstract of Html element kinds.
-
-```haxe
-enum abstract ElementKind {
+/**
+    @see https://html.spec.whatwg.org/multipage/syntax.html#elements-2
+**/
+enum abstract ElementKind(Int) to Int {
     // Not spec compliant.
     public var Unknown = -1;
     // The spec name is `Void`, but thats a Haxe keyword.
@@ -14,4 +14,3 @@ enum abstract ElementKind {
     public var Foreign;
     public var Normal;
 }
-```
---
+++ test/Main.hx
@@ -0,0 +1,11 @@
+package ;
+
+import be.html.ElementKind;
+
+class Main {
+
+    public static function main() {
+        trace( Unknown, Empty, Normal );
+    }
+
+}
